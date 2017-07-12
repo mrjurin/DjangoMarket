c6eb7bb154b9f46bb37b60aa8a69bd4fa6c0eff4
RomanPryima <pryima@stryi.com.ua>
2017-07-12 17:18:35 +0300

    added initial model for orders

--- apps/core/models.py
+++ apps/core/models.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    article = models.CharField(max_length=45)
+    description = models.TextField(max_length=450)
+    quantity = models.FloatField()
+    price = models.DecimalField()
+    order_date = models.DateTimeField()
+    customer = models.CharField(max_length=100)
+    payment_cash = models.BooleanField(null=True, blank=True)
+
+    def __str__(self):
+        return self.article
