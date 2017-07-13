c16875c436ea27a775efebbf1963454e604c01d0
stryjko <maryan.partyka@gmail.com>
2017-07-13 15:15:55 +0300

    Update models.py
    fixed model Cart

--- apps/core/models.py
+++ apps/core/models.py
@@ -3,24 +3,36 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.db import models
+from django.utils.translation import ugettext as _
 
 
+class BaseModel(models.Model):
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    delete_date = models.DateTimeField(null=True, blank=True, db_index=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["pk"]
+
+    def __unicode__(self):
+        return self.__str__()
+
+    
 class Cart(models.Model):
 
     # Cart Model
-
-    class Meta(object):
-        verbose_name = u"Cart"
     
     create_date = models.DateTimeField(auto_now_add=True, verbose_name=_('Creation Date'))
     place_order = models.BooleanField(default=False, verbose_name=_('Place Order'))
 
-    cart_id = models.CharField(max_length=50)
-    orders = models.ManyToMnyaField(Order)
-    products = models.ManyToMnyaField(Product)
+    orders = models.ManyToManyField(Order)
+    products = models.ManyToManyField(Product)
     coupon = models.OneToOneField(Coupon, primary_key=True)
-    subtotal = models.DecimalField(max_digits=50, decimal_places=2, verbose_name="Subtotal")
+    subtotal_amount = models.DecimalField(max_digits=50, decimal_places=2, verbose_name=_("Subtotal"))
 
-
+    class Meta(object):
+        verbose_name = _("Cart")
+        
     def __unicode__(self):
         return self.cart_id
