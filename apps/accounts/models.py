5fb390c59672217af44931f3109e7052e63e1b0e
Sasha1152 <saxin@ukr.net>
2017-07-14 16:18:26 +0300

    fixed code

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -7,27 +7,39 @@ from django.db import models
 
 
 class User(AbstractUser):
-    email = models.EmailField('Email address', unique=True, blank=True)
+    name = models.CharField(max_length=60)
+    email = models.EmailField('Email address', unique=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     class Meta:
+        ordering = ['name']
         verbose_name = 'User'
         verbose_name_plural = 'Users'
 
     def __unicode__(self):
-        return self.email
+        return self.name
+
+    def save(self, *args, **kwargs):
+        User.save(*args, **kwargs)
 
 
-class Account(User):
-    login = models.CharField(max_length=20, blank=True)
-    password = models.CharField(max_length=20, blank=True)
-    name = models.CharField(max_length=60, blank=True)
-    mobile_number = models.CharField(max_length=12, blank=True)
-    address = models.CharField(max_length=100)
+class UserData(User):
+
     GENDER_CHOICES = ("male", "female")
-    gender = models.CharField(choices=GENDER_CHOICES)
+
+    login = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    mobile_number = models.CharField(max_length=12, blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    photo = models.ImageField(upload_to='accounts/photo', height_field=120, width_field=160)
+    gender = models.CharField(choices=GENDER_CHOICES, blank=True)
+
+    class Meta:
+        ordering = ['login']
+        verbose_name = "userdata"
+        verbose_name_plural = "userdata"
 
     def __unicode__(self):
         return self.login
