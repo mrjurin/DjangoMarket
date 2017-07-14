309abc1a6f02d0a792fcf71acdc10d27d777d45e
RomanPryima <pryima@stryi.com.ua>
2017-07-14 11:55:02 +0300

    removed weight field

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -12,7 +12,6 @@ class Order(models.Model):
 # FIXME: PryimaRoman: insert location of other models
     additional_information = models.TextField(max_length=450)
     quantity = models.PositiveIntegerField(null=True, blank=True)
-    weight = models.FloatField(null=True, blank=True)
     price = models.DecimalField(
         max_digits=10, decimal_places=2, verbose_name="Full price")
     created_date = models.DateTimeField(auto_now_add=True)
