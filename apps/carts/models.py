f32864fa99fffc52a5e891888469afa646cb624f
Dmitry Lyapun <dlyapun@gmail.com>
2017-07-16 23:14:34 +0300

    Update models.py

--- apps/carts/models.py
+++ apps/carts/models.py
@@ -15,12 +15,24 @@ class Cart(BaseModel):
     # Cart Model
     
     create_date = models.DateTimeField(auto_now_add=True, verbose_name=_('Creation Date'))
+      
+      # для чього це поле?
     place_order = models.BooleanField(default=False, verbose_name=_('Place Order'))
 
+    # поле називається Ордер, але чомусь мени-ту-мени.
+    # Для чого взагалі це поле?
     order = models.ManyToManyField(Order)
+      
+    # Поле продукт, але продуктів може бути багато, краще робити продуктс
     product = models.ManyToManyField(Product)
+      
+    # кількість чього? 
     quantity = models.PositiveIntegerField(verbose_name=_("Quantity"))
+     
+    # Не зрозуміло що ми тут будемо зберігати?
     unit_price = models.DecimalField(max_digits=50, decimal_places=2, verbose_name=_("Unit Price"))
+   
+    # Ось це гуд
     shipping_cost = models.DecimalField(max_digits=50, decimal_places=2, verbose_name=_("Shipping Cost"))
     tax_cost = models.DecimalField(max_digits=50, decimal_places=2, verbose_name=_("TAX"))
     subtotal_amount = models.DecimalField(max_digits=50, decimal_places=2, verbose_name=_("Subtotal"))
@@ -30,4 +42,5 @@ class Cart(BaseModel):
         ordering = ['create_date']
         
     def __unicode__(self):
+        # Поля карт_ід вже нема
         return self.cart_id
