bfa69ca5c66142445eb1fe6c30d0e9b8291b67cc
Dmitry Lyapun <dlyapun@gmail.com>
2017-07-16 23:09:53 +0300

    Update models.py

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -25,21 +25,33 @@ class User(AbstractUser):
     def save(self, *args, **kwargs):
         User.save(*args, **kwargs)
 
-
+# Не розумію чого не добавити усі ці поля в модель Юзер? Навіщо створювати ще одну?
 class UserData(models.Model):
 
+    # Можна зберітаги і повни назви, а не скорочені, та питання, для чього нам ця інфа взагалі?
     GENDER_CHOICES = (('MA',"male"), ('FE', "female"))
 
+    # Чому поле називаєтясь емейл?
     email = models.ForeignKey('User')
+    
+    # Що таке логін?
     login = models.CharField(max_length=20, unique=True)
+    
     password = models.CharField(max_length=20)
+    
+    # +38(067)1111111 - довжина 16 символів
     mobile_number = models.CharField(max_length=12, blank=True)
+    
+    # Как сортування по містам роботи
     address = models.CharField(max_length=100, blank=True)
+    
+    
     photo = models.ImageField(upload_to='accounts/photo', height_field=120, width_field=160)
     gender = models.CharField(max_length=6, choices=GENDER_CHOICES, default='')
 
     class Meta:
         ordering = ['login']
+        # Поганий вибір
         verbose_name = "userdata"
         verbose_name_plural = "userdata"
 
