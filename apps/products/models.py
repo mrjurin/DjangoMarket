754927265aa1429d848490ab9db5924d73eb75dd
Dmytro Lyapun <dlyapun@gmail.com>
2017-07-17 12:53:57 +0300

    update settings

--- apps/products/models.py
+++ apps/products/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 
-from apps.core.models import BaseModel
+from ..core.models import BaseModel
 
 
 User = get_user_model()
