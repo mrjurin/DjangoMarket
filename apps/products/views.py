b0358c44d0b0e3c6528e8e73909946beefad0f16
Катерина Уполовнікова <katedreamhealer@gmail.com>
2017-07-17 11:20:15 +0300

    Merge branch 'ITEA-add-product-views' of /Users/kateryna/PycharmProjects/itea_django/DjangoMarket with conflicts.

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,5 +1,47 @@
 from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
+
+from apps.products.models import Product, Category
 
 
 class ProductsView(TemplateView):
     template_name = "products.html"
+
+    def products(self):
+        return Product.objects.all()
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(ProductsView, self).get_context_data(**kwargs)
+    #     context['products']= Product.objects.all()
+    #     return context
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    slug_url_kwarg = 'product_slug'
+    template_name = 'product_detail.html'
+
+
+class CategoriesView(TemplateView):
+    template_name = "categories.html"
+
+    def categories(self):
+        return Category.objects.all()
+
+
+class CategoryProductsView(TemplateView):
+    template_name = "category_products.html"
+
+    def category_products(self):
+        category_slug = self.kwargs['category_slug']
+        category = Category.objects.get(slug=category_slug)
+        return category.category_products.all()
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+    slug_url_kwarg = 'category_slug'
+    template_name = 'category_detail.html'
+
+
+
--- project/urls.py
+++ project/urls.py
@@ -25,6 +25,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', admin.site.urls),
+    url(r'^products_app/', include('apps.products.urls'))
 ]
 
 # For static files
