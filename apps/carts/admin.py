18f604a434c7fb4e2f0d86281eb493a4dc4063f8
stryjko <maryan.partyka@gmail.com>
2017-07-20 21:00:22 +0300

    model

--- apps/carts/admin.py
+++ apps/carts/admin.py
@@ -5,8 +5,16 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Cart
+from .models import Cart, ShippingCost, Product
 
-# Де кастом?
 
-admin.site.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    # list_display = ['id', 'Product.name', 'unit_price', 'quantity', 'ShippingCost.shipping_name', 'ShippingCost.value',
+    #                 'tax_cost', 'subtotal_amount']
+    # list_filter = ['product']
+    # search_fields = ['product']
+    pass
+
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(ShippingCost)
--- apps/carts/models.py
+++ apps/carts/models.py
@@ -16,27 +16,34 @@ class Cart(BaseModel):
     
     create_date = models.DateTimeField(auto_now_add=True, verbose_name=_('Creation Date'))
       
-      # для чього це поле? - щоб мати флажок чи в карті натиснута кнопка - прийняти замовлення чи ні
-    place_order = models.BooleanField(default=False, verbose_name=_('Place Order'))
-
-    # поле називається Ордер, але чомусь мени-ту-мени.
-    # Для чого взагалі це поле? - якщо не потрібно тоді я плутаю що таке у нас карт і що таке замовлення
-    order = models.ForeignKey(Order)
-      
-    # Поле продукт, але продуктів може бути багато, краще робити продуктс
+    # place_order = models.BooleanField(default=False, verbose_name=_('Place Order'))
+     
     products = models.ManyToManyField(Product, related_name='products')
       
-    # кількість чього? - к-сть одного продукту в кошику
     quantity = models.PositiveIntegerField(verbose_name=_("Quantity"))
      
-    # Не зрозуміло що ми тут будемо зберігати? - ціна продукту з к-стю 1
     unit_price = models.DecimalField(max_digits=50, decimal_places=2, verbose_name=_("Unit Price"))
    
     # Ось це гуд
-    shipping_cost = models.DecimalField(max_digits=50, decimal_places=2, verbose_name=_("Shipping Cost"))
+    shipping_costs = models.ManyToManyField("ShippingCost", 
+        related_name="shipping_costs", verbose_name=_("Shipping Cost"))
     tax_cost = models.DecimalField(max_digits=50, decimal_places=2, verbose_name=_("TAX"))
     subtotal_amount = models.DecimalField(max_digits=50, decimal_places=2, verbose_name=_("Subtotal"))
 
     class Meta(object):
         verbose_name = _("Cart")
         ordering = ['create_date']
+
+
+class ShippingCost(models.Model):
+    """
+    AttributeValue contains value of a specified AttributeName.
+    For ex if AttributeName is 'display diagonal' value can be 13".
+    Relation between Product and AttributeValues is MtM.
+    """
+
+    shipping_name = models.CharField(max_length=250, blank=True, null=True)
+    value = models.DecimalField(max_digits=50, decimal_places=2)
+
+    def __str__(self):
+        return "{} - {}".format(self.attribute_name.name, self.value)
