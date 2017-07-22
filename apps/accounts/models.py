fccb5897b982396f965c620919b67cbd6e322a8b
Sasha1152 <saxin@ukr.net>
2017-07-22 13:29:53 +0300

    homework

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -10,16 +10,18 @@ class User(AbstractUser):
 
     GENDER_CHOICES = (('UN', 'unknown'), ('MA', "male"), ('FE', "female"))
 
-    def default_username(self):
-        return 'user#{}'.format % self.id
+    def default_username(user):
+        return 'user#{}'.format(user.id)
 
-    username = models.CharField(max_length=50, unique=True)
-    # username = models.CharField(default=default_username(),
-    #                             max_length=50, unique=True)
+    username = models.CharField(default=default_username,
+                                max_length=50, unique=True)
     password = models.CharField(default='00', max_length=256, blank=True)
     mobile_number = models.CharField(max_length=13, blank=True)  # +380671111111 - 13 signs
     home_address = models.CharField(max_length=100, blank=True)
-    photo = models.ImageField(upload_to='accounts/photo', height_field=120, width_field=160, blank=True)
+    photo = models.ImageField(upload_to='accounts/photo',
+                              height_field=120,
+                              width_field=160,
+                              blank=True)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='')
 
     class Meta:
